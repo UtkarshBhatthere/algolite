b35d10f4b5b8773432bd5dd6cdbf66fa62b0fa13
Utkarsh Bhatt <u00584460@china.huawei.com>
2020-10-24 16:28:29 +0530

    Added Infra Code.

---
+++ sorting/ctrlflag.h
@@ -0,0 +1,14 @@
+/**********************************************************************************************************************
+ * MIT License
+ * Copyright (c) 2020 Utkarsh Bhatt
+ * @About: Contains the Macros required to control the required portions of code during compilation.
+ **********************************************************************************************************************/
+
+// Just put 1 in front of each required flag's #if.
+#if 1
+    #define __DEBUG
+#endif
+
+#if 0
+    #define __INFO
+#endif
--- sorting/infra.h
+++ sorting/infra.h
@@ -6,6 +6,9 @@
 #ifndef _INFRA_H_
 #define _INFRA_H_
 
+#include <stdlib.h>
+#include <time.h>
+
 typedef enum {
     true = 1,
     True = true,
@@ -13,45 +16,72 @@ typedef enum {
     False = false
 } bool;
 
+typedef enum {
+    success = 1,
+    failure = !success
+} status;
+
 // Usage iswap(&a, &b) -> where a and b are two integers.
-void iswap(int* a, int* b) {
+void iswap(int* a, int* b)
+{
     int temp;
     temp = *a;
     *a = *b;
     *b = temp;
 }
 
-bool isSorted_inc(int* unsrt_bfr, const long size) {
+void ifillRandom(int* bfr, const long size)
+{
+    // Seeding the pseudo-random number generator.
+    srand(time(0));
+
+    for (int i = 0; i < size; i++) {
+        bfr[i] = rand();
+    }
+}
+
+bool isSorted_inc(int* unsrt_bfr, const long size)
+{
     bool flag = true;
 
     for (int i =0; i < size - 1; i++) {
         if (unsrt_bfr[i] > unsrt_bfr[i+1]) {
             flag = false;
+            
+            #ifdef __INFO
+                printf("Fault at %d\n", i);
+            #endif
         }
 
-        if(flag == false)
-            break;
+        // if(flag == false)    // Break at first fault.
+        //     break;
     }
 
     return flag;
 }
 
-bool isSorted_dec(int* unsrt_bfr, const long size) {
+bool isSorted_dec(int* unsrt_bfr, const long size)
+{
     bool flag = true;
 
     for (int i =0; i < size - 1; i++) {
         if (unsrt_bfr[i] < unsrt_bfr[i+1]) {
             flag = false;
+
+            #ifdef __INFO
+                printf("Fault at %d\n", i);
+            #endif
         }
 
-        if(flag == false)
-            break;
+        // if(flag == false)    // Break at first fault.
+        //     break;
     }
 
     return flag;
 }
 
-void iprint_array(int* unsrt_bfr, const long size) {
+void iprint_array(int* unsrt_bfr, const long size)
+{
     for (int i = 0; i < size; i++) {
         printf("%dth : %d\n", i, unsrt_bfr[i]);
     }
