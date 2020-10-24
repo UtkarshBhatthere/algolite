700b170a7ae10deeb9302f2be4f810b476d10bfa
Utkarsh Bhatt <u00584460@china.huawei.com>
2020-10-24 16:28:10 +0530

    modifed testMain.

--- sorting/testMain.c
+++ sorting/testMain.c
@@ -1,30 +1,51 @@
 #include <stdio.h>
-#include "bubblesort.h"
+#include "ctrlflag.h"
 #include "infra.h"
+#include "timer.h"
+
+// Sorting Libraries.
+#include "bubblesort.h"
+#include "selectsort.h"
+#include "rec_bubblesort.h"
+
 
 int main() {
-    int arr[100];
-    for (int i = 100; i > 0; i--) {
-        arr[100 - i] = 101 - i;
-    }
-
-    // Uncomment if printing array is required.
-    // iprint_array(arr, 100);
+    int arr[1000];
+    clock_t *timepoint;
+    double timeElapsed;
+    
+    ifillRandom(arr, 1000);
 
     // Check if the array is already sorted. Uncomment according to need.
-    // printf("is Sorted (inc): %d\n", (int)isSorted_inc(arr, 100));
-    printf("is Sorted (dec): %d\n", (int)isSorted_dec(arr, 100));
+    printf("is Sorted (inc): %d\n", (int)isSorted_inc(arr, 1000));
+    printf("is Sorted (dec): %d\n", (int)isSorted_dec(arr, 1000));
 
-    // Increasing BubbleSort.
-    // ibubblesort_inc(arr, 100);
-    ibubblesort_dec(arr, 100);
+    // Start the timer.
+    createTimepoint(&timepoint);
 
-    // Uncomment if printing array is required.
-    // iprint_array(arr, 100);
+    // BubbleSort.
+    // ibubblesort_inc(arr, 1000);
+    ibubblesort_dec(arr, 1000);
+
+    // SelectionSort.
+    iselectsort_inc(arr, 1000);
+    // iselectsort_dec(arr, 1000);
+
+    // Recursive BubbleSort.
+    // ibubblesort_inc_rec(arr, 1000);
+    ibubblesort_dec_rec(arr, 1000);
+
+    // End timer.
+    endTimerFine(timepoint, &timeElapsed);
+
+    #ifdef __DEBUG
+        iprint_array(arr, 1000);
+    #endif
     
     // Check if the array is sorted after process. Uncomment according to need.
-    // printf("is Sorted (inc): %d\n", (int)isSorted_inc(arr, 100));
-    printf("is Sorted (dec): %d\n", (int)isSorted_dec(arr, 100));
+    // printf("is Sorted (inc): %d\n", (int)isSorted_inc(arr, 1000));
+    printf("is Sorted (dec): %d\n", (int)isSorted_dec(arr, 1000));
+    printf("Time elapsed in sorting is: %f", timeElapsed);
     return 0; 
 }
 
