b7d34c321b484dc8c2fcf46faf542a6d45eaa4f5
Utkarsh Bhatt <u00584460@china.huawei.com>
2020-11-01 04:22:06 +0530

    Fixed the filename bug in elogs.

--- sorting/elogs.h
+++ sorting/elogs.h
@@ -4,7 +4,7 @@
  * @About: Library to provide a logging process in the host program to easily generate cleaner logs.
  **********************************************************************************************************************/
 
-#ifndef _ELOGS_H_
+#ifndef _E_LOGS_H_
 #define _E_LOGS_H_
 
 #include <stdio.h>
@@ -22,13 +22,13 @@
 
 /*  Enumerations  */
 typedef enum {
-    info,
-    debug_low,
-    debug_med,
-    debug_high,
-    warn,
-    error,
-    fatal
+    info = 0,
+    debug_low = 1,
+    debug_med = 2,
+    debug_high = 3,
+    warn = 4,
+    error = 5,
+    fatal = 6
 } LoggingLevel;
 
 typedef enum {
@@ -44,6 +44,7 @@ typedef struct {
 
 /*  Global vars  */
 // Vector table which contains the LogLevel -> LogLevelName relationship.
+// TODO: Fix some structs showig (null);
 LogVector g_LogLevelTable[] =
 {
     info, "INFO",
@@ -67,21 +68,169 @@ typedef struct {
     const int   line;
 } LogOrigin;
 
+Status activateLogs()
+{
+    if (g_log_switch == NULL)
+        return failure;
+
+    int curr_state = (int)(*g_log_switch);
+    *g_log_switch = True;
+
+    // LOG_INFO("Activated Log Service: State(%d) -> State(%d)", curr_state, True);
+
+    return success;
+}
+
+Status deactivateLogs()
+{
+    if (g_log_switch == NULL)
+        return failure;
+
+    // if (True == (int)(*g_log_switch))
+    //     LOG_INFO("Deactivated Log Service: State(%d) -> State(%d)", True, False);
+
+    *g_log_switch = False;
+
+    return success;
+}
+
+Bool isLogActivated()
+{
+    if (g_log_switch == NULL)
+        return False;
+
+    return *g_log_switch;   // Switch State.
+}
+
+Status initLogModule(DateStyle date_style)
+{
+    g_log_filename = (char *)malloc((LOG_MAX_FILENAME_SIZE+1)*sizeof(char));
+    g_log_switch = (Bool *)malloc(sizeof(Bool));
+    if (g_log_filename == NULL || g_log_switch == NULL)
+        return failure;
+
+    memset(g_LogLevelTable, 0, LOG_MAX_FILENAME_SIZE);      // Clear String.
+    memset(g_log_switch, True, sizeof(Bool));               // Set to True.
+    
+    time_t start_time = time(NULL);
+    struct tm *time_info = localtime(&start_time);
+    if (start_time == -1 || time_info == NULL)
+        return failure;
+
+    const char* log_filename_fmt = "[%02d-%02d-%04d](%02d-%02d-%02d).log";     // "Date::Time"
+    
+    int chnum;
+    if (date_style == ddmmyyyy)
+        chnum = snprintf(g_log_filename, LOG_MAX_FILENAME_SIZE, log_filename_fmt, time_info->tm_mday, time_info->tm_mon,
+                 1900+time_info->tm_year, time_info->tm_hour, time_info->tm_min, time_info->tm_sec);
+    else if (date_style == mmddyyyy)
+        chnum = snprintf(g_log_filename, LOG_MAX_FILENAME_SIZE, log_filename_fmt, time_info->tm_mon, time_info->tm_mday,
+                 1900+time_info->tm_year, time_info->tm_hour, time_info->tm_min, time_info->tm_sec);
+    
+    activateLogs();     // Activating log_switch;
+
+    return success;
+}
+
+Status exitLogModule()
+{
+    // free log filename memory.
+    free(g_log_filename);
+    g_log_filename = NULL;
+
+    // free log switch memory.
+    free(g_log_switch);
+    g_log_switch = NULL;
+}
+
+const char* getLogLevelName(LoggingLevel level)
+{
+    int num_levels = sizeof(g_LogLevelTable)/sizeof(LogVector);
+    for (int i = 0; i < num_levels; i++) {
+        if(level == g_LogLevelTable[i].level){
+            // TODO: Remove this.
+            printf("level(%d), Tag(%s)\n", g_LogLevelTable[i].level, g_LogLevelTable[i].levelTag);
+            return g_LogLevelTable[i].levelTag;
+        }
+    }
+
+    return g_LogLevelTable[0].levelTag;
+}
+
+Status fillLogStamp(LogOrigin *names, const char* log_level, char **stamp_bfr)
+{
+    char* log_stamp_fmt = "[%02d.%02d.%02d][%s][%s in %s:%d] -> ";
+    *stamp_bfr = (char *)malloc((LOG_STAMP_MAX_SIZE+1)* sizeof(char));
+
+    if (NULL == *stamp_bfr)
+        return failure;
+
+    memset(*stamp_bfr, 0, LOG_STAMP_MAX_SIZE+1);
+
+    time_t curr_time = time(NULL);
+    struct tm *time_info = localtime(&curr_time);
+    
+    int chnum = snprintf(*stamp_bfr, LOG_STAMP_MAX_SIZE, log_stamp_fmt, time_info->tm_hour, time_info->tm_min,
+                         time_info->tm_sec, log_level, names->function, names->filename+2, names->line);   // +2 to skip the ./ in filename.
+    
+    if(chnum < 1)
+        return failure;
+
+    return success;
+}
+
+Status __elog(LogOrigin names, LoggingLevel level, const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    char *log_stamp = NULL;
+    int stamp_size;
+    char log_bfr[4096] = "\0";
+
+    Status ret = failure;
+    // TODO: Remove this.
+    printf("PASSED LEVEL IS %d\n", level);
+    const char* log_tag = getLogLevelName(level);
+
+    ret = fillLogStamp(&names, log_tag, &log_stamp);      // Step 1: Fill Stamp Info.
+    if (success != ret)
+        return failure;
+
+    stamp_size = strlen(log_stamp);
+
+    memcpy(log_bfr, log_stamp, stamp_size);                     // Step 2: Place the stamp to the log beginning.
+    vsnprintf(log_bfr+stamp_size, LOG_MAX_SIZE, fmt, args);     // Step 3: Place the actual log after stamp.
+
+    FILE* logFile = fopen(g_log_filename, "a");                 // Step 4: Open LogFile in append mode.
+    if(logFile == NULL){
+        // TODO: Remove this.
+        printf("File pointer is NULL errno(%d)\n", errno);
+        return failure;
+    }
+
+    fprintf(logFile, "%s\n", log_bfr);                          // Step 5: Fill log.
+    fclose(logFile);                                            // Step 6: Close File.
+
+    va_end(args);
+
+    return success;
+}
+
 #define LOG_INIT(date_style)  do {          \
     initLogModule(date_style);              \
-}
+} while (0)
 
 #define LOG_EXIT()  do {                    \
     exitLogModule();                        \
-}
+} while (0)
 
 #define LOG_ACTIVATE()  do {                \
     activateLogs();                         \
-}
+} while (0)
 
 #define LOG_DEACTIVATE()  do {              \
     deactivateLogs();                       \
-}
+} while (0)
 
 #define LOG_INFO(msg, ...)  do {                \
     if (isLogActivated() == True) {             \
@@ -159,140 +308,5 @@ typedef struct {
     __elog(name, fatal, msg, ##__VA_ARGS__);    \
     }                                           \
 } while (0)
-
-Status initLogModule(DateStyle date_style)
-{
-    g_log_filename = (char *)malloc(LOG_MAX_FILENAME_SIZE*sizeof(char));
-    g_log_switch = (Bool *)malloc(sizeof(Bool));
-    if (g_log_filename == NULL || g_log_switch == NULL)
-        return failure;
-
-    memset(g_LogLevelTable, 0, LOG_MAX_FILENAME_SIZE);      // Clear String.
-    memset(g_log_switch, True, sizeof(Bool));               // Set to True.
-    
-    time_t start_time = time(NULL);
-    struct tm *time_info = localtime(start_time);
-    if (start_time == -1 || time_info == NULL)
-        return failure;
-
-    const char* log_filename_fmt = "%02d/%02d/%04d::%02d.%02d.%02d.log";     // "Date::Time"
-
-    if (date_style == ddmmyyyy)
-        snprintf(g_log_filename, sizeof(g_log_filename), log_filename_fmt, time_info->tm_mday, time_info->tm_mon,
-                 1900+time_info->tm_year, time_info->tm_hour, time_info->tm_min, time_info->tm_sec);
-    else if (date_style == mmddyyyy)
-        snprintf(g_log_filename, sizeof(g_log_filename), log_filename_fmt, time_info->tm_mon, time_info->tm_mday,
-                 1900+time_info->tm_year, time_info->tm_hour, time_info->tm_min, time_info->tm_sec);
-
-    activateLogs();     // Activating log_switch;
-
-    return success;
-}
-
-Status activateLogs()
-{
-    if (g_log_switch == NULL)
-        return failure;
-
-    int curr_state = (int)(*g_log_switch);
-    *g_log_switch = True;
-
-    LOG_INFO("Activated Log Service: State(%d) -> State(%d)", curr_state, True);
-
-    return success;
-}
-
-Status deactivateLogs()
-{
-    if (g_log_switch == NULL)
-        return failure;
-
-    if (True == (int)(*g_log_switch))
-        LOG_INFO("Deactivated Log Service: State(%d) -> State(%d)", True, False);
-
-    *g_log_switch = False;
-
-    return success;
-}
-
-Bool isLogActivated()
-{
-    if (g_log_switch == NULL)
-        return False;
-
-    return *g_log_switch;   // Switch State.
-}
-
-Status exitLogModule()
-{
-    // free log filename memory.
-    free(g_log_filename);
-    g_log_filename = NULL;
-
-    // free log switch memory.
-    free(g_log_switch);
-    g_log_switch = NULL;
-}
-
-const char* getLogLevelName(LoggingLevel level)
-{
-    int num_levels = sizeof(g_LogLevelTable)/sizeof(LogVector);
-    for (int i = 0; i < num_levels; i++) {
-        if(level == g_LogLevelTable[i].level)
-            return g_LogLevelTable[i].levelTag;
-    }
-
-    return g_LogLevelTable[0].levelTag;
-}
-
-Status fillLogStamp(LogOrigin *names, const char* log_level, char **stamp_bfr)
-{
-    char* log_stamp_fmt = "[%02d.%02d.%02d][%s][%s in %s:%d] -> ";
-    *stamp_bfr = (char *)malloc((LOG_STAMP_MAX_SIZE+1)* sizeof(char));
-
-    if (NULL == *stamp_bfr)
-        return failure;
-
-    memset(*stamp_bfr, 0, LOG_STAMP_MAX_SIZE+1);
-
-    time_t curr_time = time(NULL);
-    struct tm *time_info = localtime(&curr_time);
-    
-    int chnum = snprintf(*stamp_bfr, LOG_STAMP_MAX_SIZE, log_stamp_fmt, time_info->tm_hour, time_info->tm_min,
-                         time_info->tm_sec, log_level, names->function, names->filename+2, names->line);   // +2 to skip the ./ in filename.
-    
-    if(chnum < 1)
-        return failure;
-
-    return success;
-}
-
-Status __elog(LogOrigin names, LoggingLevel level, const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    char *log_stamp = NULL;
-    int stamp_size;
-    char log_bfr[4096] = "\0";
-
-    Status ret = failure;
-    const char* log_tag = getLogLevelName(level);
-
-    ret = fillLogStamp(&names, log_tag, &log_stamp);      // Step 1: Fill Stamp Info.
-    if (success != ret)
-        return failure;
-
-    stamp_size = strlen(log_stamp);
-
-    memcpy(log_bfr, log_stamp, stamp_size);                     // Step 2: Place the stamp to the log beginning.
-    vsnprintf(log_bfr+stamp_size, LOG_MAX_SIZE, fmt, args);     // Step 3: Place the actual log after stamp.
-
-    FILE* logFile = fopen(g_log_filename, "a");                 // Step 4: Open LogFile in append mode.
-    fprintf(logFile, "%s\n", log_bfr);                          // Step 5: Fill log.
-    fclose(logFile);                                            // Step 6: Close File.
-
-    va_end(args);
-
-    return success;
-}
+// TODO: Fix log tag null error for INFO and DBG modes.
 #endif /*  _E_LOGS_H_  */
--- sorting/logTest.c
+++ sorting/logTest.c
@@ -6,14 +6,25 @@
 
 void doit(void)
 {
-    
-    LOG_INFO("Hello %s, this is log number %d", "World", 1);
-    LOG_DBGL("Hello %s, this is log number %d", "World", 2);
-    LOG_DBGM("Hello %s, this is log number %d", "World", 3);
-    LOG_DBGH("Hello %s, this is log number %d", "World", 4);
-    LOG_WARN("Hello %s, this is log number %d", "World", 5);
-    LOG_EROR("Hello %s, this is log number %d", "World", 6);
-    LOG_FATL("Hello %s, this is log number %d", "World", 7);
+    LOG_INIT(ddmmyyyy);
+
+    LOG_INFO("Easy Logs module is working -> %d", 1);
+    LOG_DBGL("Low level debug logging is working -> %d", 2);
+    LOG_DBGM("Med level debug logging is working -> %d", 3);
+    LOG_DBGH("High level debug logging is working -> %d", 4);
+    LOG_WARN("Warn logging is working -> %d", 5);
+    LOG_EROR("Error logging is working -> %d", 6);
+    LOG_FATL("Fatal logging is working -> %d", 7);
+
+    LOG_DEACTIVATE();
+
+    LOG_INFO("This shouldn't be in logs -> %d", 0);
+
+    LOG_ACTIVATE();
+
+    LOG_INFO("This should be in logs -> %d", 8);
+
+    LOG_EXIT();
 }
 
 int main(void)
