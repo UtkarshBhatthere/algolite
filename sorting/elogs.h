8a6c8cc240826bce5848abdf6f9a048dad42c01b
Utkarsh Bhatt <u00584460@china.huawei.com>
2020-11-01 02:41:39 +0530

    Changed logging from printf to File basd.

--- sorting/elogs.h
+++ sorting/elogs.h
@@ -12,11 +12,15 @@
 #include <time.h>
 #include <stdarg.h>
 
+
 #include "infra.h"
 
+/*  Global Constants  */
 #define LOG_STAMP_MAX_SIZE 256
 #define LOG_MAX_SIZE 4096
+#define LOG_MAX_FILENAME_SIZE 30
 
+/*  Enumerations  */
 typedef enum {
     info,
     debug_low,
@@ -27,11 +31,19 @@ typedef enum {
     fatal
 } LoggingLevel;
 
+typedef enum {
+    ddmmyyyy,
+    mmddyyyy
+} DateStyle;
+
+/*  Structures  */
 typedef struct {
     LoggingLevel level;
     const char* levelTag;
 } LogVector;
 
+/*  Global vars  */
+// Vector table which contains the LogLevel -> LogLevelName relationship.
 LogVector g_LogLevelTable[] =
 {
     info, "INFO",
@@ -43,74 +55,184 @@ LogVector g_LogLevelTable[] =
     fatal, "FATL"
 };
 
+// Logfile name dynamically generated for each session.
+char *g_log_filename = NULL;
+
+// Log ON/OFF
+Bool *g_log_switch = NULL;
+
 typedef struct {
     const char* function;
     const char* filename;
     const int   line;
 } LogOrigin;
 
-#define LOG_INFO(msg, ...)  do {            \
-    LogOrigin name = {                      \
-        .filename = __FILE__,               \
-        .function = __func__,               \
-        .line     = __LINE__                \
-    };                                      \
-    __elog(name, info, msg, ##__VA_ARGS__); \
-    } while (0)                             \
+#define LOG_INIT(date_style)  do {          \
+    initLogModule(date_style);              \
+}
 
-#define LOG_DBGL(msg, ...)  do {                    \
-    LogOrigin name = {                              \
-        .filename = __FILE__,                       \
-        .function = __func__,                       \
-        .line     = __LINE__                        \
-    };                                              \
-    __elog(name, debug_low, msg, ##__VA_ARGS__);    \
-    } while (0)                                     \
+#define LOG_EXIT()  do {                    \
+    exitLogModule();                        \
+}
 
-#define LOG_DBGM(msg, ...)  do {                    \
-    LogOrigin name = {                              \
-        .filename = __FILE__,                       \
-        .function = __func__,                       \
-        .line     = __LINE__                        \
-    };                                              \
-    __elog(name, debug_med, msg, ##__VA_ARGS__);    \
-    } while (0)                                     \
+#define LOG_ACTIVATE()  do {                \
+    activateLogs();                         \
+}
 
-#define LOG_DBGH(msg, ...)  do {                    \
-    LogOrigin name = {                              \
-        .filename = __FILE__,                       \
-        .function = __func__,                       \
-        .line     = __LINE__                        \
-    };                                              \
-    __elog(name, debug_high, msg, ##__VA_ARGS__);   \
-    } while (0)                                     \
+#define LOG_DEACTIVATE()  do {              \
+    deactivateLogs();                       \
+}
 
-#define LOG_WARN(msg, ...)  do {            \
-    LogOrigin name = {                      \
-        .filename = __FILE__,               \
-        .function = __func__,               \
-        .line     = __LINE__                \
-    };                                      \
-    __elog(name, warn, msg, ##__VA_ARGS__); \
-    } while (0)                             \
+#define LOG_INFO(msg, ...)  do {                \
+    if (isLogActivated() == True) {             \
+        LogOrigin name = {                      \
+            .filename = __FILE__,               \
+            .function = __func__,               \
+            .line     = __LINE__                \
+        };                                      \
+        __elog(name, info, msg, ##__VA_ARGS__); \
+    }                                           \
+} while (0)
 
-#define LOG_EROR(msg, ...)  do {                 \
-    LogOrigin name = {                          \
-        .filename = __FILE__,                   \
-        .function = __func__,                   \
-        .line     = __LINE__                    \
-    };                                          \
-    __elog(name, error, msg, ##__VA_ARGS__);    \
-    } while (0)                                 \
+#define LOG_DBGL(msg, ...)  do {                        \
+    if (isLogActivated() == True) {                     \
+        LogOrigin name = {                              \
+            .filename = __FILE__,                       \
+            .function = __func__,                       \
+            .line     = __LINE__                        \
+        };                                              \
+        __elog(name, debug_low, msg, ##__VA_ARGS__);    \
+    }                                                   \
+} while (0)
+
+#define LOG_DBGM(msg, ...)  do {                        \
+    if (isLogActivated() == True) {                     \
+        LogOrigin name = {                              \
+            .filename = __FILE__,                       \
+            .function = __func__,                       \
+            .line     = __LINE__                        \
+        };                                              \
+        __elog(name, debug_med, msg, ##__VA_ARGS__);    \
+    }                                                   \
+} while (0)
+
+#define LOG_DBGH(msg, ...)  do {                        \
+    if (isLogActivated() == True) {                     \
+        LogOrigin name = {                              \
+            .filename = __FILE__,                       \
+            .function = __func__,                       \
+            .line     = __LINE__                        \
+        };                                              \
+        __elog(name, debug_high, msg, ##__VA_ARGS__);   \
+    }                                                   \
+} while (0)
+
+#define LOG_WARN(msg, ...)  do {                    \
+    if (isLogActivated() == True) {                 \
+        LogOrigin name = {                          \
+            .filename = __FILE__,                   \
+            .function = __func__,                   \
+            .line     = __LINE__                    \
+        };                                          \
+        __elog(name, warn, msg, ##__VA_ARGS__);     \
+    }                                               \
+} while (0)
+
+#define LOG_EROR(msg, ...)  do {                    \
+    if (isLogActivated() == True) {                 \
+        LogOrigin name = {                          \
+            .filename = __FILE__,                   \
+            .function = __func__,                   \
+            .line     = __LINE__                    \
+        };                                          \
+        __elog(name, error, msg, ##__VA_ARGS__);    \
+    }                                               \
+} while (0)
 
 #define LOG_FATL(msg, ...)  do {                \
-    LogOrigin name = {                          \
-        .filename = __FILE__,                   \
-        .function = __func__,                   \
-        .line     = __LINE__                    \
-    };                                          \
+    if (isLogActivated() == True) {             \
+        LogOrigin name = {                      \
+            .filename = __FILE__,               \
+            .function = __func__,               \
+            .line     = __LINE__                \
+        };                                      \
     __elog(name, fatal, msg, ##__VA_ARGS__);    \
-    } while (0)                                 \
+    }                                           \
+} while (0)
+
+Status initLogModule(DateStyle date_style)
+{
+    g_log_filename = (char *)malloc(LOG_MAX_FILENAME_SIZE*sizeof(char));
+    g_log_switch = (Bool *)malloc(sizeof(Bool));
+    if (g_log_filename == NULL || g_log_switch == NULL)
+        return failure;
+
+    memset(g_LogLevelTable, 0, LOG_MAX_FILENAME_SIZE);      // Clear String.
+    memset(g_log_switch, True, sizeof(Bool));               // Set to True.
+    
+    time_t start_time = time(NULL);
+    struct tm *time_info = localtime(start_time);
+    if (start_time == -1 || time_info == NULL)
+        return failure;
+
+    const char* log_filename_fmt = "%02d/%02d/%04d::%02d.%02d.%02d.log";     // "Date::Time"
+
+    if (date_style == ddmmyyyy)
+        snprintf(g_log_filename, sizeof(g_log_filename), log_filename_fmt, time_info->tm_mday, time_info->tm_mon,
+                 1900+time_info->tm_year, time_info->tm_hour, time_info->tm_min, time_info->tm_sec);
+    else if (date_style == mmddyyyy)
+        snprintf(g_log_filename, sizeof(g_log_filename), log_filename_fmt, time_info->tm_mon, time_info->tm_mday,
+                 1900+time_info->tm_year, time_info->tm_hour, time_info->tm_min, time_info->tm_sec);
+
+    activateLogs();     // Activating log_switch;
+
+    return success;
+}
+
+Status activateLogs()
+{
+    if (g_log_switch == NULL)
+        return failure;
+
+    int curr_state = (int)(*g_log_switch);
+    *g_log_switch = True;
+
+    LOG_INFO("Activated Log Service: State(%d) -> State(%d)", curr_state, True);
+
+    return success;
+}
+
+Status deactivateLogs()
+{
+    if (g_log_switch == NULL)
+        return failure;
+
+    if (True == (int)(*g_log_switch))
+        LOG_INFO("Deactivated Log Service: State(%d) -> State(%d)", True, False);
+
+    *g_log_switch = False;
+
+    return success;
+}
+
+Bool isLogActivated()
+{
+    if (g_log_switch == NULL)
+        return False;
+
+    return *g_log_switch;   // Switch State.
+}
+
+Status exitLogModule()
+{
+    // free log filename memory.
+    free(g_log_filename);
+    g_log_filename = NULL;
+
+    // free log switch memory.
+    free(g_log_switch);
+    g_log_switch = NULL;
+}
 
 const char* getLogLevelName(LoggingLevel level)
 {
@@ -165,11 +287,12 @@ Status __elog(LogOrigin names, LoggingLevel level, const char *fmt, ...)
     memcpy(log_bfr, log_stamp, stamp_size);                     // Step 2: Place the stamp to the log beginning.
     vsnprintf(log_bfr+stamp_size, LOG_MAX_SIZE, fmt, args);     // Step 3: Place the actual log after stamp.
 
-    printf("%s\n", log_bfr);
+    FILE* logFile = fopen(g_log_filename, "a");                 // Step 4: Open LogFile in append mode.
+    fprintf(logFile, "%s\n", log_bfr);                          // Step 5: Fill log.
+    fclose(logFile);                                            // Step 6: Close File.
 
     va_end(args);
 
     return success;
 }
-// TODO: Implement a way to dump the logs to a file.
 #endif /*  _E_LOGS_H_  */
